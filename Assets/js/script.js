7c247b38a84e49c861bef9ac9788e28e738242be
Hikmet Dogrul <hikmetdogrul00@gmail.com>
2021-06-27 17:34:47 -0400

    adding persistent data

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,3 +5,22 @@ var convertedDate = moment(today, randomFormat);
 
 var currentDay = document.getElementById("currentDay");
 currentDay.textContent = convertedDate.format('dddd, MMMM Do');
+
+var textBoxes = document.getElementsByTagName("input");
+var saveButtons = document.querySelectorAll(".fa-save");
+saveButtons.forEach(saveButton => saveButton.addEventListener("click", function () {
+
+    for (let index = 0; index < textBoxes.length; index++) {
+        if (textBoxes[index].value !== null || textBoxes[index].value !== '') {
+            window.localStorage.setItem(['textBox' + index], textBoxes[index].value);
+        }
+    }
+}));
+
+if(typeof(Storage) !== "undefined") {
+    for (let index = 0; index < textBoxes.length; index++) {
+        textBoxes[index].value = window.localStorage.getItem(['textBox' + index]);
+    }
+} else {
+    console.info("This page is not reloaded");
+}
