0f28c7064f8d551b349bb7ccf95f782eef0428c5
Hikmet Dogrul <hikmetdogrul00@gmail.com>
2021-06-27 21:38:10 -0400

    adding css for present past and future

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,12 +2,15 @@ var today = new Date();
 // Convert the `randomDate` to the "MM/DD/YYYY" format using `moment()`
 var randomFormat = 'MM/DD/YYYY';
 var convertedDate = moment(today, randomFormat);
+var currentHour = 10//today.getHours();
+var workingHours = document.querySelectorAll(".hour");
+var textBoxes = document.getElementsByTagName("input");
+var saveButtons = document.querySelectorAll(".fa-save");
 
 var currentDay = document.getElementById("currentDay");
 currentDay.textContent = convertedDate.format('dddd, MMMM Do');
 
-var textBoxes = document.getElementsByTagName("input");
-var saveButtons = document.querySelectorAll(".fa-save");
+
 saveButtons.forEach(saveButton => saveButton.addEventListener("click", function () {
 
     for (let index = 0; index < textBoxes.length; index++) {
@@ -16,11 +19,24 @@ saveButtons.forEach(saveButton => saveButton.addEventListener("click", function 
         }
     }
 }));
-
-if(typeof(Storage) !== "undefined") {
+if (typeof (Storage) !== "undefined") {
     for (let index = 0; index < textBoxes.length; index++) {
         textBoxes[index].value = window.localStorage.getItem(['textBox' + index]);
     }
 } else {
     console.info("This page is not reloaded");
+}
+
+
+for (let index = 0; index < workingHours.length; index++) {
+
+    if (currentHour < workingHours[index].id) {
+        textBoxes[index].className = textBoxes[index].className + " future";
+       
+    } else if (currentHour > workingHours[index].id) {
+        textBoxes[index].className = textBoxes[index].className + " past";
+    } else {
+        textBoxes[index].className = textBoxes[index].className + " present";
+        
+    }
 }
