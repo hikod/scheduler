cc771bff18993b0a5ee462bd60647efa6f104682
Hikmet Dogrul <hikmetdogrul00@gmail.com>
2021-06-27 21:39:15 -0400

    removing hard coded value

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,7 +2,7 @@ var today = new Date();
 // Convert the `randomDate` to the "MM/DD/YYYY" format using `moment()`
 var randomFormat = 'MM/DD/YYYY';
 var convertedDate = moment(today, randomFormat);
-var currentHour = 10//today.getHours();
+var currentHour = today.getHours();
 var workingHours = document.querySelectorAll(".hour");
 var textBoxes = document.getElementsByTagName("input");
 var saveButtons = document.querySelectorAll(".fa-save");
